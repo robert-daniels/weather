1993eb8bfea9762e6ad3e5e372a984bf1ae352a2
Robert <robert.daniels.vc@gmail.com>
2022-07-16 11:51:36 -0500

    add weather api call

--- app.js
+++ app.js
@@ -12,14 +12,37 @@ geoLocationURL = `http://api.positionstack.com/v1/forward` +
                  `&query=${addressString}` +
                  `&limit=1`
 
+// request(
+//     {
+//         url: geoLocationURL,
+//         json: true
+//     },
+//     (error, response) => {
+//         data = response.body.data[0];
+//         console.log(`Longitude: ${data.longitude}`);
+//         console.log(`Latitude: ${data.latitude}`);
+//     }
+// )
+
+weatherURL = `http://api.weatherstack.com/current` +
+             `?access_key=${apiKeys.keyWeatherAPI}` +
+             `&query=40.7831,-73.9712`
+
 request(
     {
-        url: geoLocationURL,
+        url: weatherURL,
         json: true
     },
     (error, response) => {
-        data = response.body.data[0];
-        console.log(`Longitude: ${data.longitude}`);
-        console.log(`Latitude: ${data.latitude}`);
+        if (error) {
+            console.log('Unable to connect to weather service! Please check connectivty and try again later');
+        } else {
+            data = response.body.current;
+            for (let property in data) {
+                console.log(`${property}: ${data[property]}`);
+            }
+        }
+        
+        
     }
 )
