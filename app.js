d310d45f3551119ba6f5d7de8aacbf91cc99f9a7
Robert <robert.daniels.vc@gmail.com>
2022-07-16 12:07:06 -0500

    add error handling, geo and weather api

--- app.js
+++ app.js
@@ -12,37 +12,48 @@ geoLocationURL = `http://api.positionstack.com/v1/forward` +
                  `&query=${addressString}` +
                  `&limit=1`
 
-// request(
-//     {
-//         url: geoLocationURL,
-//         json: true
-//     },
-//     (error, response) => {
-//         data = response.body.data[0];
-//         console.log(`Longitude: ${data.longitude}`);
-//         console.log(`Latitude: ${data.latitude}`);
-//     }
-// )
+request(
+    {
+        url: geoLocationURL,
+        json: true
+    },
+    (error, response) => {
+        if (error) {
+            console.log('Unable to connect to location services. Please check connectivity and try again later');
+        } 
+        else if (response.body.error){
+            console.log('Unable to find given location. Try again with different search terms');
+        }
+        else {
+            data = response.body.data[0];
+            console.log(`Longitude: ${data.longitude}`);
+            console.log(`Latitude: ${data.latitude}`);
+        }
+    }
+)
 
 weatherURL = `http://api.weatherstack.com/current` +
              `?access_key=${apiKeys.keyWeatherAPI}` +
              `&query=40.7831,-73.9712`
 
-request(
-    {
-        url: weatherURL,
-        json: true
-    },
-    (error, response) => {
-        if (error) {
-            console.log('Unable to connect to weather service! Please check connectivty and try again later');
-        } else {
-            data = response.body.current;
-            for (let property in data) {
-                console.log(`${property}: ${data[property]}`);
-            }
-        }
+// request(
+//     {
+//         url: weatherURL,
+//         json: true
+//     },
+//     (error, response) => {
+//         if (error) {
+//             console.log('Unable to connect to weather service! Please check connectivty and try again later');
+//         } else if (response.body.error){
+//             console.log(`Unable to find weather for given location`);
+//         } 
+//         else {
+//             data = response.body.current;
+//             for (let property in data) {
+//                 console.log(`${property}: ${data[property]}`);
+//             }
+//         }
         
         
-    }
-)
+//     }
+// )
