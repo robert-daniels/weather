a9560e7ca916eee1dbef984251367a5f59294d13
Robert <robert.daniels.vc@gmail.com>
2022-07-16 18:29:23 -0500

    refactor util functionality

--- app.js
+++ app.js
@@ -29,37 +29,3 @@ forecast.getForecast(40.7831, -73.9712, (error, data) => {
 
 
 
-
-
-
-
-
-
-
-
-
-// weatherURL = `http://api.weatherstack.com/current` +
-//              `?access_key=${apiKeys.keyWeatherAPI}` +
-//              `&query=40.7831,-73.9712`
-
-// request(
-//     {
-//         url: weatherURL,
-//         json: true
-//     },
-//     (error, response) => {
-//         if (error) {
-//             console.log('Unable to connect to weather service! Please check connectivty and try again later');
-//         } else if (response.body.error){
-//             console.log(`Unable to find weather for given location`);
-//         } 
-//         else {
-//             data = response.body.current;
-//             for (let property in data) {
-//                 console.log(`${property}: ${data[property]}`);
-//             }
-//         }
-        
-        
-//     }
-// )
